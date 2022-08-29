05200b636f0898bbbceab67f259d1c910e727937
shiseihuang <shisei.huang@bric.jp>
2022-08-29 15:30:29 +0900

    add cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_QBKS7jfLc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3c2phh76i6p4rnr4oageki7pmc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
